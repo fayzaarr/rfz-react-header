9be1a30240580e12c75572bb204714956b9bc8a3
fayzaarr <nurfayza.rr@gmail.com>
2024-10-28 16:43:10 +0700

    initial scnd commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { Component } from 'react';
 import $ from 'jquery'
 import Header from './Components/Header';
+import About from './Components/About';
 
 class App extends Component {
   constructor(props) {
@@ -42,6 +43,7 @@ class App extends Component {
     return(
     <div className="App">
       <Header data={this.state.resumeData.main} />
+      <About data={this.state.resumeData.main}/>
     </div>
     )
   }
