b7e4c8842a6a78857180ca4562d6fcd46453ad0d
fayzaarr <nurfayza.rr@gmail.com>
2024-10-28 22:08:23 +0700

    projects commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './Components/Header';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Resume from './Components/Resume';
+import Portfolio from './Components/Portfolio';
 
 class App extends Component {
   constructor(props) {
@@ -46,8 +47,9 @@ class App extends Component {
     <div className="App">
       <Header data={this.state.resumeData.main} />
       <About data={this.state.resumeData.main}/>
-      <Contact data={this.state.resumeData.main}/>
       <Resume data={this.state.resumeData.resume}/>
+      <Portfolio data={this.state.resumeData.portfolio}/>
+      <Contact data={this.state.resumeData.main}/>
     </div>
     )
   }
